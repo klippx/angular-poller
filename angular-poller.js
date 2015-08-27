a79faad61c710a8598a98df98f2c632b58df7058
Mathias Klippinge <mathias.klippinge@apoex.se>
2015-08-27 11:13:04 +0200

    Added polling service

--- angular-poller.js
+++ angular-poller.js
@@ -5,7 +5,17 @@
  * License: MIT
  */
 
- angular.module('your-name.my-directive', [])
-  .directive('btfMyDirective', function () {
-    // implementation goes here
-  });
+angular.module('angular-poller', [])
+  .service('pollingService', [
+    '$timeout', function($timeout) {
+      this.setTimeout = function(scope, fn, delay) {
+        var deregister, promise;
+        promise = $timeout(fn, delay);
+        deregister = scope.$on('$destroy', function() {
+          return $timeout.cancel(promise);
+        });
+        return promise.then(deregister);
+      };
+      return this;
+    }
+  ]);
