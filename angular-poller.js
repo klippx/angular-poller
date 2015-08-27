452c54dfca865379c541d10d6fdedf26bac3bf4a
Mathias Klippinge <mathias.klippinge@apoex.se>
2015-08-27 08:21:48 +0200

    Initial commit

---
+++ angular-poller.js
@@ -0,0 +1,11 @@
+/*
+ * @license
+ * angular-poller v0.0.0
+ * (c) 2015 Mathias Klippinge http://klippx.github.io/
+ * License: MIT
+ */
+
+ angular.module('your-name.my-directive', [])
+  .directive('btfMyDirective', function () {
+    // implementation goes here
+  });
